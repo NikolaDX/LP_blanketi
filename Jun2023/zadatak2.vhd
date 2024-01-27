8d5182e2ee98f5b8621cabb4633691cd19d9b9bd
NikolaDX <68640891+NikolaDX@users.noreply.github.com>
2024-01-27 15:36:02 +0100

    Ispravljen zadatak 2

--- Jun2023/zadatak2.vhd
+++ Jun2023/zadatak2.vhd
@@ -8,9 +8,12 @@ entity Seq_Det_Behavioral is
 end entity;
 
 architecture Seq_Det_Behavioral_arch of Seq_Det_Behavioral is
-	type State_Type is (S0, S1, S2, S3);
+	subtype State_Type is std_logic_vector(3 downto 0);
+	constant S0 : State_Type := "0001";
+	constant S1 : State_Type := "0010";
+	constant S2 : State_Type := "0100";
+	constant S3 : State_Type := "1000";
 	signal current_state, next_state : State_Type;
-
 begin
 
 	STATE_MEMORY : process (Clock, Reset)
@@ -45,6 +48,7 @@ begin
 							else
 								next_state <= S1;
 							end if;
+				when others => next_state <= S0;
 			end case;
 	end process;
 
@@ -52,9 +56,7 @@ begin
 		begin
 			case (current_state) is
 				when S0 => Dout <= '1';
-				when S1 => Dout <= '0';
-				when S2 => Dout <= '0';
-				when S3 => Dout <= '0';
+				when others => Dout <= '0';
 			end case;
 	end process;
 
